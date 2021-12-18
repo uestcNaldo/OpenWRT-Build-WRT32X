9c55704d382a40c0700d3d5d2a885dcce2880961
Naldo Wang <1165648703@qq.com>
2021-12-18 15:15:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-gti xrayui https://github.com/yichya/luci-app-xray' >>feeds.conf.default
+echo 'src-git xrayui https://github.com/yichya/luci-app-xray' >>feeds.conf.default
